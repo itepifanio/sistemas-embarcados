91ae203208c2d5d059c0eb41e26f2a3b7956cb77
Epitácio Bessa da Silva <bessa.epitacio@gmail.com>
2021-09-14 21:01:03 -0300

    Endpoint na APi para receber as atualizações das filas

--- servidor-b/routes/api.php
+++ servidor-b/routes/api.php
@@ -14,4 +14,5 @@ Route::middleware('auth:sanctum')->get('/que', function (Request $request) {
 Route::name('api.')
     ->group(function () {
         Route::get('queue-status/{id}', StatusStatsController::class)->name('queue-status');
+        Route::post('queue-status/', [StatusStatsController::class, 'store'])->name('queue-status-store');
     });
